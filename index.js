2f6701aa0ed4cd1477144d5eb2f469bcf17437d7
Scott Robinson <scott.robinson2@sky.uk>
2023-04-05 11:31:33 +0100

    Solve jokes task from w1 of backend module

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const { Op } = require('sequelize');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get('/jokes', async (req, res, next) => {
-  try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
-  } catch (error) {
-    console.error(error);
-    next(error)
-  }
+app.get('/jokes', async (req, resp) => {
+    /*
+    2. A fully complete GET /jokes route should:
+
+    Return a list of all jokes when no query string parameters are provided
+
+    Return a list of jokes, filtered by tag for GET /jokes?tag=YOUR_QUERY_HERE
+
+    Return a list of jokes, filtered by content GET /jokes?content=YOUR_QUERY_HERE
+    */
+    const { tags, content } = req.query;
+    let jokes;
+
+    let whereQuery = {}
+    let tagsQuery;
+    let contentQuery;
+    if (tags) tagsQuery = {tags: {[Op.substring]: tags}};
+    if (content) contentQuery = {joke: {[Op.substring]: content}};
+    
+    if (tagsQuery && contentQuery) whereQuery = {[Op.and]: {...tagsQuery, ...contentQuery}};
+    else whereQuery = tagsQuery ?? contentQuery;
+
+    jokes = await Joke.findAll({where: whereQuery});
+    resp.json(jokes);
 });
 
 // we export the app, not listening in here, so that we can run tests
