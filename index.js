81b452bf05c604b020e7805ec2d74528df9ed803
Scott Robinson <scott.robinson2@sky.uk>
2023-04-05 12:05:39 +0100

    Complete bonus task (POST, PUT, DELETE)

--- index.js
+++ index.js
@@ -32,5 +32,67 @@ app.get('/jokes', async (req, resp) => {
     resp.json(jokes);
 });
 
+/*
+BONUS TASK FOR EXTRA CREDIT:
+POST /jokes: Adds a joke to the database. Should accept both columns in the req.body
+
+PUT /jokes/:id: Edits a joke by ID.  Should accept both/either columns in the req.body
+
+DELETE /jokes/:id: Removes a joke from the database, by ID.
+
+*/
+
+app.post("/jokes", async (req, resp) => {
+    const { tags, content } = req.body;
+    if (tags === undefined) {
+        resp.status(400).send("Missing 'tags' from body");
+        return;
+    }
+    if (content === undefined) {
+        resp.status(400).send("Missing 'content' from body");
+        return;
+    }
+
+    await Joke.create({tags, joke: content});
+    resp.sendStatus(201);
+});
+
+app.put("/jokes/:id", async (req, resp) => {
+    const { id } = req.params
+    const {tags, content} = req.body
+
+    if (tags === undefined) {
+        resp.status(400).send("Missing the replacement tags of the task");
+        return;
+    }
+    if (content === undefined) {
+        resp.status(400).send("Missing the replacement content of the task");
+        return;
+    }
+
+    const updated = await Joke.update({tags, joke: content}, {where: {id}});
+    console.log(updated)
+    if (updated[0] === 0) {
+        // The joke doesn't exist, so create it instead
+        await Joke.create({tags, joke: content});
+        console.log("created")
+        resp.sendStatus(201);
+        return;
+    }
+    resp.sendStatus(200);
+});
+
+app.delete("/jokes/:id", async (req, resp) => {
+    const { id } = req.params;
+
+    const deleted = await Joke.destroy({where: {id}});
+    console.log(deleted);
+    if (deleted === 0) {
+        resp.status(404).send("Joke Not Found");
+        return;
+    }
+    resp.sendStatus(204);
+})
+
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
